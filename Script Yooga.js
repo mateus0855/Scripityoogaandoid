ef8a81b8ee2a3b86f301fcf2671ba679703fb3f1
mateus0855 <mateuschessuis@gmail.com>
2026-03-21 01:39:36 -0300

    Update script version to 66.2

--- Script Yooga.js
+++ Script Yooga.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Confirmar Pedido Yooga - V66.1
-// @version      66.1
+// @name         Confirmar Pedido Yooga - V66.2
+// @version      66.2
 // @description  Bloqueio por nome Mateus + Auto-update via GitHub.
 // @author       Seu Nome
 // @match        *://app.yooga.com.br/*

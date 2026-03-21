ee30ce2244d9179184a05b0ff59a946677c9554b
mateus0855 <mateuschessuis@gmail.com>
2026-03-21 02:20:01 -0300

    Add user script metadata for Confirmar Pedido Yooga
    Initial creation of the user script metadata for Confirmar Pedido Yooga.

---
+++ scriptyooga.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Confirmar Pedido Yooga - V70
+// @version      70.0
+// @description  Versão Meta para iPhone Safari
+// @author       Mateus
+// @match        *://app.yooga.com.br/*
+// @match        *://confirmacao-entrega-propria.ifood.com.br/*
+// @updateURL    https://raw.githubusercontent.com/mateus0855/Scripityoogaandoid/main/scriptyooga.meta.js
+// @downloadURL  https://raw.githubusercontent.com/mateus0855/Scripityoogaandoid/main/scriptyooga.user.js
+// @grant        none
+// ==/UserScript==

1a776ba8362cf2f47248694be877275e3bfc2dcf
mateus0855 <mateuschessuis@gmail.com>
2026-05-18 15:29:00 -0300

    Update Yooga script metadata to version 87.0
    Updated script metadata for Yooga version 87.0, removing button group and maintaining print system.

--- scriptyooga.meta.js
+++ scriptyooga.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Confirmar Pedido Yooga - V70.3
-// @version      70.3
-// @description  Versão Meta para iPhone Safari
+// @name         Confirmar Pedido Yooga - V87.0 (Remoção do Button Group + Autoreset)
+// @version      87.0
+// @description  Baseado na V86.0. Remove o grupo de botões de ações do delivery e mantém o sistema inteligente de print e persistência de rotas.
 // @author       Mateus
 // @match        *://app.yooga.com.br/*
 // @match        *://confirmacao-entrega-propria.ifood.com.br/*

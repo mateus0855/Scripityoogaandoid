6152d45bc5699ce525cc25fecf062193e86585fe
mateus0855 <mateuschessuis@gmail.com>
2026-05-28 13:44:08 -0300

    Update script version and name to V88.3

--- scriptyooga.user.js
+++ scriptyooga.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Confirmar Pedido Yooga - V88.2 (Correção bug mola 2 ihpone)
-// @version      88.2
+// @name         Confirmar Pedido Yooga - V88.3 (Correção bug mola ihpone)
+// @version      88.3
 // @description  Baseado na V87.0. Remove o grupo de botões de ações do delivery e mantém o sistema inteligente de print e persistência de rotas.
 // @author       Mateus
 // @match        *://app.yooga.com.br/*
